2c9284f6553d6e4ef4c89729b213dc9298c82d25
shaju george <shajugeorge45@gmail.com>
2020-07-20 00:53:24 +0530

    added password window

--- passwords.py
+++ passwords.py
@@ -1,29 +1,68 @@
 from tkinter import *
-
-window= Tk()
-
-window.title(' my accouts')
-window.geometry('500x500')
-
-def store():
-    name = site_e.get()
-    site_e.delete(0,'end')
-    pas=password_e.get()
-    password_e.delete(0,'end')
-    mylabel=Label(window,text=" site :"+name + " password :" + pas,font=('Times', 18))
-    mylabel.grid(row=4 , columnspan =3)
-
-site= Label(window, text='site',font=('Times', 18))
-site_e= Entry(window,font=('Times', 18),width = 30,borderwidth=10)
-password= Label(window, text='password',font=('Times', 18))
-password_e= Entry(window, font=('Times', 18),width = 30,borderwidth=10)
-submit=Button(window, text='submit',font=('Times', 18),command=store)
-
-site.grid(row=0,column=0,sticky= W)
-password.grid(row=1,column=0,sticky= E)
-site_e.grid(row=0,column=1)
-password_e.grid(row=1,column=1)
-submit.grid(row=2,column=1,padx=30,sticky= S,pady =15)
+from tkinter import messagebox
 
 
-window.mainloop()
+
+#password window
+
+class password():
+    def __init__(self):
+        self.root=Tk()
+        self.pass_label = Label(self.root,text="password", font=('Arial', 14))
+        self.e=Entry(self.root, font=('Arial', 14))
+        self.sub=Button(self.root,text='verify', font=('Arial', 14),bg="white",command=self.check)
+        self. pass_label.pack()
+        self.e.pack(pady=10)
+        self.sub.pack()
+        
+       
+        self.root.mainloop()
+        
+    def check(self):
+      self.value = self.e.get()
+      # print(self.value)
+      if "sha" == self.value:
+
+        self.root.destroy()
+#user window
+
+        global i
+        i = 4
+
+        def store():
+            name = site_e.get()
+            site_e.delete(0,'end')
+            pas=password_e.get()
+            password_e.delete(0,'end')
+            
+            if True:
+                global i
+                mylabel=Label(window,text=" site :"+name + " password :" + pas,font=('Times', 18))
+                mylabel.grid(row=i , columnspan =3)
+                i+=1
+
+        window= Tk()
+        window.title(' my accouts')
+        window.geometry('500x500')
+        site= Label(window, text='site',font=('Times', 18))
+        site_e= Entry(window,font=('Times', 18),width = 30,borderwidth=10)
+        password= Label(window, text='password',font=('Times', 18))
+        password_e= Entry(window, font=('Times', 18),width = 30,borderwidth=10)
+        submit=Button(window, text='submit',font=('Times', 18),command=store)
+
+        site.grid(row=0,column=0,sticky= W)
+        password.grid(row=1,column=0,sticky= E)
+        site_e.grid(row=0,column=1)
+        password_e.grid(row=1,column=1)
+        submit.grid(row=2,column=1,padx=30,sticky= S,pady =15)
+
+
+        window.mainloop()
+      else:
+        messagebox.showinfo(' wrong password ')
+p=password()
+
+
+
+
+
